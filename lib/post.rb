908f18a22bd7c514878e569754bd663d55fccad5
George Ruiz <georuiz817@gmail.com>
2019-06-26 16:31:26 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,9 @@
 class Post 
-  attr_accessor :name :author  
+  attr_accessor :name,:author  
   
   def intitialize(name)
+    def initialize(name)
+  @name = name 
+  @artist = artist 
+  @@all << self 
+end
